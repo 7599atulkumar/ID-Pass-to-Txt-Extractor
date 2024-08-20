80acfd35f95895a86adde1eb7e73a5f7c22db530
7599atulkumar <168633003+7599atulkumar@users.noreply.github.com>
2024-08-21 02:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29768900
-api_hash = "6a56e5c2383dc7b85210febe60b9f229"
-bot_token = "7416646489:AAEYBOjSuR5nY5-wGBSXqJmI2foHhzjR4Jo"
-auth_users = "5986670447, 1406300051, 6801378994, 870101201"
+api_id = 20491966
+api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
+bot_token = "6889907679:AAH-X2uHMM5j1EVJ2iQE5WGjNbzwfRtI5FM"
+auth_users = "5986670447, 1406300051, 6801378994, 870101201, 7168441486"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "5986670447"
+osowner_users = "7168441486"
 owner_users = [int(num) for num in osowner_users.split(",")]
